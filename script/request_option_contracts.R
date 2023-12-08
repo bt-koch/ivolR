965d84735da5e43a1f25547b8a27af39083c4320
bt-koch <bela.koch@hotmail.com>
2023-12-08 15:41:19 +0100

    remove shortened indexing used for testing

--- script/request_option_contracts.R
+++ script/request_option_contracts.R
@@ -1,14 +1,12 @@
 rm(list=ls()); gc()
 devtools::load_all()
 
-path_symbols <- "/Volumes/SanDisk64gb/ivolR"
+path_symbols <- "/Users/belakoch/Documents/coding/ivolR output/ad hoc cs"
 output_path <- file.path(path_symbols, "option contracts")
 from <- "2022-01-01"
 to <- "2023-06-30"
 
-
-
-for (file in list.files(path_symbols, pattern = "option_symbol_.*\\.rds")[1:2]) {
+for (file in list.files(path_symbols, pattern = "option_symbol_.*\\.rds")) {
 
   # extract ticker
   df <- readRDS(file.path(path_symbols, file))
@@ -22,9 +20,10 @@ for (file in list.files(path_symbols, pattern = "option_symbol_.*\\.rds")[1:2]) 
   tickers <- df$optionSymbol
 
   res <- list()
+  i <- 1
 
-  for (tckr in tickers[1:2]) {
-    cat(paste0("\nRequest ", tckr, "... "))
+  for (tckr in tickers) {
+    cat(paste0("\n", i, " of ", length(tickers), ": Request ", tckr, "... "))
     req <- request(
       endpoint = "equities/eod/single-stock-option",
       symbol = tckr,
@@ -33,6 +32,7 @@ for (file in list.files(path_symbols, pattern = "option_symbol_.*\\.rds")[1:2]) 
     )
     res[[tckr]] <- req
     cat("finished!")
+    i <- i+1
   }
 
   output_name <- paste0("option_contract_", stock)
