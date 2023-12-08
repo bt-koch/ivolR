3c53fde96cd9832344299df6e1ea486cf317d003
bt-koch <bela.koch@hotmail.com>
2023-12-08 15:39:39 +0100

    convert wrds csv

--- R/datahandling.R
+++ R/datahandling.R
@@ -22,3 +22,60 @@ manipulate <- function(file) {
   return(option_chains)
 
 }
+
+options_csv_to_list <- function(csv_file, to_ivol_format = T) {
+
+  df <- read.csv(csv_file)
+  option_identifier <- "symbol"
+  stock_identifier <- "ticker"
+
+  if (to_ivol_format) {
+    df <- data.frame(
+      symbol = df$ticker,
+      exchange = df$exchange_d,
+      date = df$date,
+      `Adjusted close` = NA_real_,
+      `option symbol` = df$symbol,
+      expiration = df$exdate,
+      strike = df$strike_price,
+      `Call/Put` = df$cp_flag,
+      style = df$exercise_style,
+      ask = df$best_offer,
+      bid = df$best_bid,
+      volume = df$volume,
+      `open interest` = df$open_interest,
+      Unadjusted = NA_real_,
+      check.names = F
+    )
+    option_identifier <- "option symbol"
+    stock_identifier <- "symbol"
+  }
+
+  df$stock_identifier <- df[[stock_identifier]]
+
+  output <- list()
+
+  for (tckr in unique(df[[stock_identifier]])) {
+
+    lst <- list()
+
+    df_tckr_subset <- subset(df, stock_identifier == tckr)
+    df_tckr_subset$chain_id <- sub("^(.*?\\d{6}[CP]).*", "\\1", df_tckr_subset[[option_identifier]])
+
+    for (id in unique(df_tckr_subset$chain_id)) {
+
+      temp <- subset(df_tckr_subset, chain_id == id)
+      temp$chain_id <- NULL
+      temp$stock_identifier <- NULL
+      lst[[id]] <- temp
+    }
+
+    output[[tckr]] <- lst
+
+  }
+
+  return(output)
+
+}
+
+
