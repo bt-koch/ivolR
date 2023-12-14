cc2516fdaef3b980802ef4069dc23ba841691a5e
bt-koch <bela.koch@hotmail.com>
2023-12-14 16:29:22 +0100

    create_optionchain -> change colnames options_csv_to_list -> change colnames enrich_optionchains -> add risk free rate function to get riskfree rate csv

--- R/datahandling.R
+++ R/datahandling.R
@@ -1,8 +1,9 @@
-manipulate <- function(file) {
+create_optionchain <- function(file) {
 
   lst <- readRDS(file)
 
   data <- lapply(lst, function(x) x$data)
+  data <- Filter(Negate(is.null), data)
   names(data) <- sub("^(.*?\\d{6}[CP]).*", "\\1", names(data))
 
   option_chains <- list()
@@ -17,6 +18,11 @@ manipulate <- function(file) {
     } else {
       option_chains[[chain_id]] <- df_option_chain[[1]]
     }
+
+    names(option_chains[[chain_id]]) <- tolower(names(option_chains[[chain_id]]))
+    names(option_chains[[chain_id]]) <- sub(" ", "_", names(option_chains[[chain_id]]))
+    colnames(option_chains[[chain_id]])[colnames(option_chains[[chain_id]]) == "stocksymbol"] <- "symbol"
+    colnames(option_chains[[chain_id]])[colnames(option_chains[[chain_id]]) == "call/put"] <- "call_put"
   }
 
   return(option_chains)
@@ -46,17 +52,17 @@ options_csv_to_list <- function(csv_file, to_ivol_format = T, add_stockprice = T
       symbol = df$ticker,
       exchange = df$exchange_d,
       date = df$date,
-      `Adjusted close` = NA_real_,
-      `option symbol` = df$symbol,
+      adjusted_close = NA_real_,
+      option_symbol = df$symbol,
       expiration = df$exdate,
       strike = df$strike_price/1000,  # on wrds: strike price * 1000
-      `Call/Put` = df$cp_flag,
+      call_put = df$cp_flag,
       style = df$exercise_style,
       ask = df$best_offer,
       bid = df$best_bid,
       volume = df$volume,
-      `open interest` = df$open_interest,
-      Unadjusted = NA_real_,
+      open_interest = df$open_interest,
+      unadjusted = NA_real_,
       check.names = F
     )
     option_identifier <- "option symbol"
@@ -79,14 +85,14 @@ options_csv_to_list <- function(csv_file, to_ivol_format = T, add_stockprice = T
     })
 
     stock_prices <- lapply(stock_prices, function(x) {
-      x$`Adjusted close` <- as.numeric(sub("\\$", "", x$Schluss.Letzter))
+      x$adjusted_close <- as.numeric(sub("\\$", "", x$Schluss.Letzter))
       return(x)
     })
 
     stock_prices <- do.call(rbind, stock_prices)
     stock_prices <- stock_prices[, c("symbol", "date", "Adjusted close")]
 
-    df$`Adjusted close` <- NULL
+    df$adjusted_close <- NULL
 
     df <- merge(x = df, y = stock_prices, by = c("symbol", "date"), all.x = T)
     df <- df[, column_order]
@@ -132,11 +138,23 @@ options_csv_to_list <- function(csv_file, to_ivol_format = T, add_stockprice = T
 #' - open interest
 #' - weight (open interest_i / sum(open interest))
 #'
+#' @param df
+#' @param rf must contain a column `date_id` in format `%Y-%m` and a column
+#'           `risk_free_rate` containing the risk free rate in decimals
+#'
 #' @return
 #' @export
 #'
 #' @examples
-enrich_optionchains <- function(df) {
+enrich_optionchains <- function(df, rf = get_riskfree("input/euribor6month.csv")) {
+
+  # wrds <- readRDS("/Users/belakoch/Documents/coding/ivolR output/option chains/wrds_SIVB.rds")
+  # wrds <- wrds[[1]]
+  #
+  # ivol <- readRDS("/Users/belakoch/Documents/coding/ivolR output/option chains/STAN.rds")
+  # ivol <- ivol[[1]]
+
+  # df <- readRDS("/Users/belakoch/Documents/coding/ivolR output/all banks/option chains/UBS.rds")[[12]]
 
   # 1. calculate time to maturity (expiration - date)
   df$date <- as.Date(df$date)
@@ -144,7 +162,7 @@ enrich_optionchains <- function(df) {
   df$time_to_maturity <- df$expiration - df$date
 
   # 2. calculate weight
-  df <- transform(df, weight = `open interest` / ave(`open interest`, date, FUN = sum),
+  df <- transform(df, weight = open_interest / ave(open_interest, date, FUN = sum),
                   check.names = F)
 
   # 3. define price
@@ -153,24 +171,39 @@ enrich_optionchains <- function(df) {
 
   # 3.2 stock price for strike price=0 (artificial contract)
   temp <- df
-  temp[, c("strike", "ask", "bid", "volume", "open interest", "price", "weight")] <- NA
-  substr(temp$`option symbol`, nchar(temp$`option symbol`)-5, nchar(temp$`option symbol`)) <- "000000"
+  temp[, c("strike", "bid", "ask", "volume", "open_interest", "price", "weight")] <- NA
+  # substr(temp$option_symbol, nchar(temp$option_symbol)-5, nchar(temp$option_symbol)) <- "000000"
+  temp$option_symbol <- NA
   temp <- unique(temp)
-  temp$price <- temp$`Adjusted close`
+  temp$price <- temp$adjusted_close
   temp$strike <- 0
   temp$weight <- 1
+
   df <- rbind(df, temp)
 
   # 4. add risk free rate (join by date)
-  # TODO get data from Darwin
-  df$risk_free_rate <- NA
+  df$date_id <- substr(df$date, 1, 7)
+  rf <- rf[, c("date_id", "risk_free_rate")]
+  rf$date_id <- substr(rf$date, 1, 7)
+  df <- merge(
+    x = df, y = rf,
+    by = "date_id",
+    all.x = T
+  )
 
   # 5. return finished df
   df <- df[, c("symbol", "date", "expiration", "time_to_maturity", "strike",
-             "price", "open interest", "weight", "risk_free_rate")]
+             "price", "open_interest", "weight", "risk_free_rate")]
   df <- df[order(df$date, df$strike), ]
   return(df)
 
 }
 
-
+get_riskfree <- function(file) {
+  df <- read.csv(file)
+  df$TIME.PERIOD <- NULL
+  names(df) <- c("date", "risk_free_rate")
+  df$risk_free_rate <- df$risk_free_rate/100
+  df$date_id <- substr(df$date, 1, 7)
+  return(df)
+}
