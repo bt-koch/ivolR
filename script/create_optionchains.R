b276b15b35717e24daabc9564129e8586a7633ea
bt-koch <bela.koch@hotmail.com>
2023-12-14 16:27:55 +0100

    track progress

--- script/create_optionchains.R
+++ script/create_optionchains.R
@@ -3,13 +3,14 @@ devtools::load_all()
 
 banks <- jsonlite::fromJSON("input/banks.json")
 
-path_in <- "/Users/belakoch/Documents/coding/ivolR output/ad hoc cs/option contracts"
-path_out <- "/Users/belakoch/Documents/coding/ivolR output/option chains"
+path_in <- "/Users/belakoch/Documents/coding/ivolR output/all banks/option contracts"
+path_out <- "/Users/belakoch/Documents/coding/ivolR output/all banks/option chains"
+i <- 1
 
 for (f in list.files(path_in)) {
-
-  data <- manipulate(file.path(path_in, f))
+  cat("\r", i, "of", length(list.files(path_in)))
+  data <- create_optionchain(file.path(path_in, f))
   ticker <- substr(f, 17, nchar(f)-4)
   saveRDS(data, file = file.path(path_out, paste0(ticker, ".rds")))
-
+  i <- i+1
 }
