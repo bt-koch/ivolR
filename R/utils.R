4f07a471fce3267be2549bb391bbd2c27a013d76
bt-koch <bela.koch@hotmail.com>
2023-12-14 16:26:19 +0100

    add function to get exchange where ticker is traded

--- R/utils.R
+++ R/utils.R
@@ -21,10 +21,30 @@ get_bank_info <- function(bank_json, bank_name) {
   }
 }
 
-get_bank_by_ticker <- function(bank_json, ticker) {
+get_bank_by_ticker <- function(banks, ticker) {
 
   return(
     banks[sapply(banks$ticker, function(x) ticker %in% x), ]
   )
 
+}
+
+get_exchange_by_ticker <- function(banks, ticker) {
+
+  df <- banks[sapply(banks$ticker, function(x) ticker %in% x), ]
+
+  idx <- which(df$ticker[[1]] == ticker)
+  exchange <- df$exchange[[1]][idx]
+
+  # somewhat stupid solution to make following condition work
+  # --> following solution is when multimatches of ticker in same region
+  # this will past together multimatches for different regions and separates
+  # them later, not elegant but works
+  if (length(exchange) > 1) exchange <- paste(exchange, collapse = "|")
+
+  if (grepl("\\|", exchange)) {
+    exchange <- unlist(strsplit(exchange, "\\|"))
+  }
+
+  return(exchange)
 }
